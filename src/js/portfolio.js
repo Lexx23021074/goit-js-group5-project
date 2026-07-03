a39c6b73c37bef43d835953b3df4345824628013
Taras <lexx23021074@gmail.com>
2026-07-03 21:08:58 +0000

    made structure of proj

---
+++ src/js/portfolio.js
@@ -0,0 +1,1 @@
+console.log('Portfolio module loaded');
---
+++ src/main.js
@@ -0,0 +1,10 @@
+import './js/about.js';
+import './js/benefits.js';
+import './js/contacts.js';
+import './js/faq.js';
+import './js/feedbacks.js';
+import './js/footer.js';
+import './js/header.js';
+import './js/hero.js';
+import './js/modal.js';
+import './js/portfolio.js';
