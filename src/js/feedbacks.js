2fd26bb49b4576779b2f3f1c1b2243cbccf730c1
Polina <polinakvyat9@gmail.com>
2026-07-07 19:52:38 +0300

    Feedbacks section

--- src/js/feedbacks.js
+++ src/js/feedbacks.js
@@ -1,1 +1,26 @@
-console.log('Feedbacks module loaded');
+const swiper = new Swiper('.testimonials-swiper', {
+    slidesPerView: 1,
+    spaceBetween: 20,
+  
+  navigation: {
+    nextEl: '.btn-next',
+    prevEl: '.btn-prev',
+  },
+
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+  },
+
+  breakpoints: {
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    }
+  }
+});
